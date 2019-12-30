4655bd8c442c3325cc62c0a182f4a40f4474764e
John Azzaro <jazzaro@temple.edu>
2019-12-29 21:16:33 -0800

    improved index.js, added notes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
+//Imports
 const express = require('express');
 const router = express.Router();
 
+
+// root index route
 router.get('/', function(req, res) {
   res.send('This is a test')
 });
 
+
+//Export
 module.exports = router;
 
