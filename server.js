60d37036c7461a4bbf88000d435c6cf799acef6f
John Azzaro <jazzaro@temple.edu>
2020-01-01 20:16:36 -0800

    fixed dev environment config issue

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 //Imports
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
@@ -9,33 +13,14 @@ mongoose.Promise = global.Promise;
 const app = express();
 
 
-//Configuration
-app.set("view engine", "ejs");
-app.set("views", __dirname + "/views");
-app.set("layout", "layouts/layout"); 
-
-
 //Module Imports
 const indexRouter = require("./routes/index");
 
 
-
-
-//Database
-mongoose.connect(process.env.DATABASE_URL, { 
-  useNewUrlParser: true,  useUnifiedTopology: true 
-});
-
-const db = mongoose.connection;
-db.on('error', function(error) {
-  console.error(error)
-});
-db.once('open', function() {
-  console.log('Connected to mongoose...');
-});
-
-
-
+//Configuration
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout"); 
 
 
 //Middleware
@@ -44,11 +29,23 @@ app.use(express.static('public'));
 app.use("/", indexRouter);
 
 
+
+//Database
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
+
+
+
+
+
+
 //Routes -- See "routes" module.
 
 
 //Server
 let server;
-server = app.listen('3000', function() {
+server = app.listen(process.env.PORT || 3000, function() {
   console.log('Your app is listening on Port 3000...');
 });
