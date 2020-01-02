856d27660f28c7b62c05fcd7276cc0b8795dc939
John Azzaro <jazzaro@temple.edu>
2020-01-01 20:17:25 -0800

    added unifiedTopology mongoose to fix dep warn

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use("/", indexRouter);
 
 
 //Database
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
