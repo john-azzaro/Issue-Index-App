bafa62bb813dfcc6ea074a4624ddb1eca635f0c5
John Azzaro <jazzaro@temple.edu>
2020-01-06 11:23:06 -0800

    potential fix for post issue

--- routes/categories.js
+++ routes/categories.js
@@ -10,18 +10,23 @@ router.get('/', function(req, res) {
   res.render('categories/index');
 });
 
+
 //New Category Route
 router.get('/new', function(req, res) {
   res.render('categories/new', { category: new Category() });
 });
+
 
 // Create Category Route
 router.post('/', function(req, res) {
   const category = new Category({
     name: req.body.name
   });
-  category.save(function () {
-    
+  category.save(function (err, newCategory) {
+    res.render('categories/new', {
+      category: category,
+      errorMessage: 'Error creating category'
+    });
   });
 
   res.send(req.body.name);
