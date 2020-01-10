9c982b7ca47b7a6c99664acf04aaf1af49549012
John Azzaro <jazzaro@temple.edu>
2020-01-09 22:26:43 -0800

    testing

--- routes/issues.js
+++ routes/issues.js
@@ -7,19 +7,19 @@ const { Issue } = require('../models/issue');
 // Routes
 // All Issues Route
 router.get('/', async function(req, res) {
-  
+  res.send('All issues works!')
 });
 
 
 //New Issue Route
 router.get('/new', function(req, res) {
- 
+  res.send('New issues works!')
 });
 
 
 // Create Issue Route
 router.post('/', async function(req, res) {
-  
+  res.send('Create issues works!')
 });
 
 
