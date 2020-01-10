46d186ff113cf4ea7c4376c3b744e872db1142e4
John Azzaro <jazzaro@temple.edu>
2020-01-09 16:59:35 -0800

    initial model format, needs revision

--- models/issue.js
+++ models/issue.js
@@ -1,9 +1,30 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
-  name: {
+  title: {
     type: String,
     required: true,
+  },
+  description: {
+    type: String,
+  },
+  issueUpdatedDate: {
+    type: Date,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    require: true,
+    default: Date.now
+  },
+  issueImage: {
+    type: String,
+
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Category'
   }
 });
 
