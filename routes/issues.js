e3c6500bacf6fac6533466c876bb3a6b19719a48
John Azzaro <jazzaro@temple.edu>
2020-01-09 16:18:45 -0800

    added issues router

---
+++ routes/issues.js
@@ -0,0 +1,48 @@
+//Imports
+const express = require('express');
+const router = express.Router();
+const { Category } = require('../models/category');
+
+
+// Routes
+// All Categories Route
+router.get('/', async function(req, res) {
+  let searchOptions = {}
+  if (req.query.name != null && req.query.name !== '') {
+    searchOptions.name = new RegExp(req.query.name, 'i')
+  }
+  try {
+    const categories = await Category.find(searchOptions);
+    res.render('categories/index', { 
+      categories: categories,
+      searchOptions: req.query
+    });
+  } catch(err) {
+    res.redirect('/');
+  }
+});
+
+
+//New Category Route
+router.get('/new', function(req, res) {
+  res.render('categories/new', { category: new Category() });
+});
+
+
+// Create Category Route
+router.post('/', async function(req, res) {
+  const category = new Category({
+    name: req.body.name
+  });
+  try {
+    const newCategory = await category.save()   
+    // res.redirect(`categories/${newCategory.id}`);
+    res.redirect(`categories`);
+  } catch(err) {
+    res.render('categories/new', {
+      category: category,
+      errorMessage: 'Error creating category'
+    });    
+  }
+});
+
