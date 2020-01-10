88f575a44a9b21382a08886514b0a420b531d9ac
John Azzaro <jazzaro@temple.edu>
2020-01-09 16:51:52 -0800

    added book model logic

---
+++ models/issue.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  }
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = { Book };
