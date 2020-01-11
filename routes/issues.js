3f6e7cc1cc009e7ca392729218b289a66f273a31
John Azzaro <jazzaro@temple.edu>
2020-01-10 21:15:30 -0800

    revised issue route logic

--- routes/issues.js
+++ routes/issues.js
@@ -15,14 +15,14 @@ router.get('/', async function(req, res) {
 //New Issue Route
 router.get('/new', async function(req, res) {
   try {
-    const categories = await Category.find({});                                           // get all categories, categories equal to await (i.e. wait until) Category model and find everything (i.e. {})
-    const issue = new Issue();                                                         // create a new Issue
-    res.render('issues/new', {                                                        // if successful, render issues/new and pass in variables
-      categories: categories,                                                             // send categories to new page and...
-      issue: issue                                                                     // send issues to the new page.
-    })                      
+    const categories = await Category.find({});
+    const issue = new Issue();
+    res.render('issues/new', {
+      categories: categories,
+      issue: issue
+    });
   } catch (err) {
-    res.redirect('/issues');                                             // if the are any errors, redirect back to the /issues page.
+    res.redirect('/issues');  
   }
   
   
