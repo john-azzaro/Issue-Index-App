7deb9529a6738294eb7e9dbe5899bb10520c9cd3
John Azzaro <jazzaro@temple.edu>
2020-01-13 20:39:40 -0800

    no issues with libraries

--- routes/issues.js
+++ routes/issues.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-// const path = require('path');
+const path = require('path');
 const { Issue } = require('../models/issue');
 const { Category } = require('../models/category');
 
