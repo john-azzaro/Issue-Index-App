2159fb1b82da23f08b8222119c2c8676f2e390da
John Azzaro <jazzaro@temple.edu>
2020-01-17 21:25:56 -0800

    async not working

--- routes/issues.js
+++ routes/issues.js
@@ -21,14 +21,13 @@ const upload = multer({
 // All Issues Route
 router.get('/', async function(req, res) {
   try {
-
+    res.render('issues/index', {                                       // render page issues/index and pass...
+      issues: issues,                                                  // ... the issues
+      searchParams: req.query                                         // and search params
+    });
   } catch(err) {
-
+    res.redirect('/');
   }
-  res.render('issues/index', {                                       // render page issues/index and pass...
-    issues: issues,                                                  // ... the issues
-    searchParams: req.query                                         // and search params
-  });
 });
 
 
