6a2a0a4bd6a3a9c9272f53e729fbc112e4e05f3c
John Azzaro <jazzaro@temple.edu>
2020-01-17 22:05:23 -0800

    fixed recent issue

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 //Imports
 const express = require('express');
 const router = express.Router();
+const Issue = require('../models/issue');
 
 
 // root index route
-router.get('/', function(req, res) {
-  res.render('index');
+router.get('/', async function(req, res) {
+  let issues;
+  try {
+    issues = await Issue.find().sort( { createdAt: 'desc' }).limit(10).exec();        // This will find all the issues in the database and sort them. exec is used to execute the code.
+  } catch(err) {
+    issues = [];
+  } 
+  res.render('index', { issues: issues });
 });
 
 
