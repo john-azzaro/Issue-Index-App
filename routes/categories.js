d40f5a7b14c0edb1fe85399a4f495cf148df9b23
John Azzaro <jazzaro@temple.edu>
2020-02-05 22:00:17 -0800

    added update route

--- routes/categories.js
+++ routes/categories.js
@@ -50,15 +50,19 @@ router.post('/', async function(req, res) {
 // Show categories
 router.get('/:id', function (req, res) {        // get route for categories, pass in "/:id" which means that an id is going to be passed along with the request.
   res.send('Show Category ' + res.params.id)     // this req.params.id on the request object will get all the parameters we defined inside the url
-})                 
+});               
 
 
 // Edit categories page
 router.get('/:id/edit', function (req, res) {           // use id again because we need it to select what we want to edit.
   res.send('Edit Category ' + res.params.id)           // this req.params.id on the request object will get all the parameters we defined inside the url
-})
+});
 
 
+// Update route page
+router.put('/:id', function(req, res) {                  // put is saying this is going to be edited so no need for edit.
+  res.send('Update Categoruy ' + res.params.id)           // this req.params.id on the request object will get all the parameters we defined inside the url
+});
 
 
 
