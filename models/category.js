c60942792f246b0ea2e2f6a62e6298afb54737cb
John Azzaro <jazzaro@temple.edu>
2020-02-18 21:17:24 -0800

    updated cat model

--- models/category.js
+++ models/category.js
@@ -7,7 +7,9 @@ const categorySchema = new mongoose.Schema({
   }
 });
 
-categorySchema.pre()
+categorySchema.pre('remove', function(next) {      // pre allows you to run an action before the method occurs, and pass "remove" and a function with the callback "next", which will mean it is ok to move forward unless it is an error.
+
+});
 
 const Category = mongoose.model('Category', categorySchema);
 
