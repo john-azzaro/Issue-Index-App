9f9ce3685a3625ab96ce607e77c34eec49c61a01
John Azzaro <jazzaro@temple.edu>
2020-02-18 21:26:59 -0800

    pre schema logic

--- models/category.js
+++ models/category.js
@@ -12,8 +12,10 @@ categorySchema.pre('remove', function(next) {
   Issue.find({category: this.id}, function (err, issues) {    // if any issues for this catgory
     if(err) {                                                 // if error...
       next(err);                                              // pass the error on (an prevent you from removing )
-    } else if (issues.length > 0) {
-      
+    } else if (issues.length > 0) {                           // for the particular issue, if there are issues for the category, do NOT remove the category.
+      next(new Error('This category still has issues'))
+    } else {                                                 // but if no errors and no issues, then
+      next();
     }
   });    
 });
