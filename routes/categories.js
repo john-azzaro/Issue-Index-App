e6668d551f1851b379fa23b47c3e0928295d2bc4
John Azzaro <jazzaro@temple.edu>
2020-02-19 22:47:09 -0800

    updated

--- routes/categories.js
+++ routes/categories.js
@@ -51,8 +51,8 @@ router.post('/', async function(req, res) {
 router.get('/:id', async function (req, res) {     
   try {
     const category = await category.findById(req.params.id);              // get category by id
-    const issue = await issues.find({category: category.id}).exec();                                   // find issues by a particular category and limit the amount you want to get with .limit(if you want to) and execute.
-  
+    const issues = await issues.find({category: category.id}).exec();                                   // find issues by a particular category and limit the amount you want to get with .limit(if you want to) and execute.
+    res.render('categories/show', {category: category, issuesByCategory: issues})                                         // if both of the above are true, then 
   } catch(err) {
     res.redirect('/');
   }
